026e2ecbea085ac3fd38136909c73b23ebde8c60
marcmaliar <marc.maliar77@gmail.com>
2024-11-13 21:13:34 -0500

    edited yaml and main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,8 @@
-ami           = "abc123"
-instance_type = "t2.micro"
+resource "aws_instance" "ubuntu2204" {
+  ami           = "ami-0ea3c35c5c3284d82"
+  instance_type = "t2.micro"
+  subnet_id     = "subnet-05eb5546ca629c7c4"
+  tags = {
+    Name = "ubuntu2204"
+  }
+}
