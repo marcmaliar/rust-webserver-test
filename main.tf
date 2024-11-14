55817288885404d8a337c12ffaa3820032bb7db8
marcmaliar <marc.maliar77@gmail.com>
2024-11-13 21:18:05 -0500

    Adding provider aws

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+provider "aws" {
+  region = "us-east-2"  # Replace with your desired AWS region
+  # Optional configurations:
+  # access_key = "your-access-key"
+  # secret_key = "your-secret-key"
+  # profile    = "your-aws-profile"
+}
+
 resource "aws_instance" "ubuntu2204" {
   ami           = "ami-0ea3c35c5c3284d82"
   instance_type = "t2.micro"
